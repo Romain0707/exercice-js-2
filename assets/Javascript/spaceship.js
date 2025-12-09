e5c3ebd7c4fafb986e12a01bed55b94591f3c415
Romain Demaiz <vipdemaiz@gmail.com>
2025-12-09 13:52:08 +0100

    [NEW] Solved problem with ship stuttering

--- assets/Javascript/spaceship.js
+++ assets/Javascript/spaceship.js
@@ -1,32 +1,35 @@
 const xWing = document.querySelector('.x-wing');
 
-let numberX = xWing.getBoundingClientRect().x;
-let numberY = xWing.getBoundingClientRect().y;
+let numberX = xWing.getBoundingClientRect().left;
+let numberY = xWing.getBoundingClientRect().top;
+
+xWing.style.left = numberX + "px";
+xWing.style.top = numberY + "px";
 
 window.addEventListener('keypress', (e) => {
-    if(e.key === "z" && numberX > 20) {
-        xWing.style.top = numberX + "px";
-        numberX += -10; 
+    if(e.key === "z" && numberY > 20) {
+        numberY -= 10; 
+        xWing.style.top = numberY + "px";
     }
 });
 
 window.addEventListener('keypress', (e) => {
-    if(e.key === "s" && numberX < window.innerHeight - 70) {
-        xWing.style.top = numberX + "px";
-        numberX += 10;
-    }
-})
-
-window.addEventListener('keypress', (e) => {
-    if(e.key === "q" && numberY > 20) {
-        xWing.style.left = numberY + "px";
-        numberY += -10;
-    }
-})
-
-window.addEventListener('keypress', (e) => {
-    if(e.key === "d" && numberY < window.innerWidth - 70) {
-        xWing.style.left = numberY + "px";
+    if(e.key === "s" && numberY < window.innerHeight - 70) {
         numberY += 10;
+        xWing.style.top = numberY + "px";
     }
-})
+});
+
+window.addEventListener('keypress', (e) => {
+    if(e.key === "q" && numberX > 20) {
+        numberX -= 10;
+        xWing.style.left = numberX + "px";
+    }
+});
+
+window.addEventListener('keypress', (e) => {
+    if(e.key === "d" && numberX < window.innerWidth - 70) {
+        numberX += 10;
+        xWing.style.left = numberX + "px";
+    }
+});
